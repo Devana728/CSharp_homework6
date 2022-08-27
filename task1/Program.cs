d96305baa53c760bb3f59899e6fb0630c89c85d3
devana <devana728@rambler.ru>
2022-08-28 01:56:39 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    System.Console.Write(message); //вывести сообщение
+    string readValue = Console.ReadLine(); //считывает с консоли строку
+    int result = int.Parse(readValue); // преобразует строку в целое число
+    return result; //возвращает результат
+}
+int[] GeneraitArray(int length)
+
+{
+    int [] array = new int[length];
+    int a = Prompt("Введите любое целое число :");
+    int b = Prompt("Введите любое целое число :");
+    int c = Prompt("Введите любое целое число :");
+    int d = Prompt("Введите любое целое число :");
+    int e = Prompt("Введите любое целое число :");
+    int i = 0;
+    while (i < array.Length)
+    {
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+        array[3] = d;
+        array[4] = e;
+        i++;
+    }
+    return array;
+}
+
+// вывод массива в строку
+void printArray(int[] array)
+{
+    int count = 0;
+    while (count < array.Length)
+    {
+        System.Console.Write($"{array[count]}\t");
+        count++;
+    }
+    System.Console.WriteLine();
+}
+int[] array1 = GeneraitArray(5);
+
+printArray(array1);
+
+int CountPositiveDigit(int[] array)
+{
+    int digit = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0)
+        {
+            
+            digit = digit + 1;
+        }
+        
+    return digit;
+}
+System.Console.WriteLine($"Количество чисел больше ноля {CountPositiveDigit(array1)}");
---
+++ task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+int Prompt(string message)
+{
+    System.Console.Write(message); //вывести сообщение
+    string readValue = Console.ReadLine(); //считывает с консоли строку
+    int result = int.Parse(readValue); // преобразует строку в целое число
+    return result; //возвращает результат
+}
+
+
+
+(double, double) CrosslineCheck(double b1, double k1, double b2, double k2)
+{
+         // y = k2*x +b2
+        //y-y = (k1-k2)x -(b1-b2);
+    double x = (b2-b1)/(k1-k2);
+    double y = k1*x +b1;
+    var result =(x,y); 
+    return result;
+    
+    
+}
+
+double k1 =  Prompt("Значение к1 : ");
+double b1 =  Prompt("Значение b1 : "); 
+double k2 =  Prompt("Значение к2 : ");
+double b2 =  Prompt("Значение b2 : ");
+CrosslineCheck( k1, b1, k2, b2);
+if ( b2 == b1 && k2 == k1)
+{
+    System.Console.WriteLine("прямые совпадают");
+}
+else
+   if(k2==k1)
+   {
+       System.Console.WriteLine("прямые параллельны");
+   }
+   else
+   {
+       (var x, var y)= CrosslineCheck( b1, k1, b2, k2);
+       System.Console.WriteLine($"прямые пересекаются в точке ({x:f2};{y:f2})");
+   }
+
+
